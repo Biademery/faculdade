14e20a240a34d9bdd13afaf5a8293ad8161887af
Biademery <biademery@gmail.com>
2022-09-13 20:40:59 -0300

    add exercise

---
+++ Aula05/atividades/04-exercícios/Q02.py
@@ -0,0 +1,8 @@
+# Solicite ao usuário um valor numérico, inteiro ou real, e escrever se é positivo ou negativo (considere o valor zero como positivo).
+
+number = int(input("Digite um número: "))
+
+if number < 0:
+    print("O número é negativo")
+elif number > 0:
+    print("O número é positivo")
---
+++ Aula05/atividades/04-exercícios/Q03.py
@@ -0,0 +1,13 @@
+# As maçãs custam 1,30 cada, se forem compradas menos de uma dúzia, e 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
+
+
+
+maçãs = float(input("Quantas maçãs você quer comprar: "))
+
+if maçãs < 12:
+    valor_maça = maçãs * 1.30
+    print(f"{valor_maça} reais")
+
+elif maçãs >= 12:
+    valor_maça = maçãs * 1
+    print(f"{valor_maça} reais")
