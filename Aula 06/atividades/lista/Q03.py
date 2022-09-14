7ef128ccafe93acb192cad0202b1cce9ac243c4c
Biademery <biademery@gmail.com>
2022-09-13 21:12:29 -0300

    add Aula06

---
+++ Aula 06/atividades/lista/Q03.py
@@ -0,0 +1,10 @@
+# Armazene os nomes de alguns de seus amigos em uma lista chamada amigos. Exiba o nome de cada pessoa acessando cada elemento da lista um de cada vez.
+
+lista_amigos = [
+    "Analicia",
+    "Pedro",
+    "João",
+    "Bia"]
+
+for nome in lista_amigos:
+    print(f"{nome}, obrigado por vir a aula!")
---
+++ Aula 06/atividades/lista/Q06a.py
@@ -0,0 +1,1 @@
+# Crie uma lista de convidados para um jantar em sua casa, com pelo menos 5 celebridades.
