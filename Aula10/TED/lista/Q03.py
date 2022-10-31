3e5f13af748c472affe06046ae7002121608d9a5
Biademery <biademery@gmail.com>
2022-10-31 17:00:28 -0300

    add Lista

--- Aula10/TED/lista/Q03.py
+++ Aula10/TED/lista/Q03.py
@@ -1,17 +1,13 @@
 # 03 - Ler um vetor Q de 20 posições (aceitar somente números positivos). Escrever a seguir:
 # A) O valor do maior elemento de Q e a respectiva posição que ele ocupa no vetor;
-
-from distutils.log import error
-
+from random import randint
 
 Q = []
 
 for i in range(20):
-    i = float(input("Digite um número positivo: "))
-    if i > 0:
-        Q.append(i)
-    else:
-        print(float(input("Digite apenas um número positivo: ")))
+    i = randint(0, 100)
+    Q.append(i)
+
 
 print(f"O valor do maior elemento é {max(Q)}, e sua posição no vetor é a {Q.index(max(Q))}")
 
--- Aula10/TED/lista/Q04.py
+++ Aula10/TED/lista/Q04.py
@@ -1,12 +1,19 @@
 # Q04 - Ler um vetor A de 10 números. Após, ler mais um número e guardar em uma variável X. 
 # Armazenar em um vetor M o resultado de cada elemento de A multiplicado pelo valor X. Logo após, 
 # imprimir o vetor M.
+from random import randint
 
 A = []
 M = []
 
-x = float(input("Digite um número para x: "))
 
 for i in range(10):
-    i = A.append(float(input("Digite um número: ")))
+    i = A.append(randint(0, 100))
+
+x = randint(0, 9)
+
+for i in range(len(A)):
+    M.append(x * A[i])
+
+print(f"M: {M}")
     
