698b883805c388f7e7ce395973e1c0c5d1e2bfe5
Bia D'Emery <biademery@gmail.com>
2024-04-16 09:07:26 -0300

    Aula 04

---
+++ Aula/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormularioDeContato from './components/Formulario';
+
+export default function App() {
+  return (
+    <div>
+      <FormularioDeContato />
+    </div>
+  );
+}
+
