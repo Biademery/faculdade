215107bd881b1c0973172e445c018ae5ba0d27b9
Bia D'Emery <biademery@gmail.com>
2024-12-01 18:33:31 -0300

    update project

--- trabalho-angelo-main/cypress/e2e/step_definitions/cidades_e_estados/cidades_e_estados.js
+++ trabalho-angelo-main/cypress/e2e/step_definitions/cidades_e_estados/cidades_e_estados.js
@@ -1,24 +1,22 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("I'm on the main IBGE page", () => {
-    cy.visit("https://ibge.gov.br/pt/inicio.html?lang=pt-BR")
-})
+  cy.visit("https://ibge.gov.br/pt/inicio.html?lang=pt-BR");
+});
 
-When('Click on combobox', () => {
-    cy.get('.chzn-single > span').click()
-})
+When("Click on combobox", () => {
+  cy.get(".chzn-single > span").click();
+});
 
-Then('Shows the search field', () => {
-    cy.get('.chzn-search > input')
-})
+Then("Shows the search field", () => {
+  cy.get(".chzn-search > input");
+});
 
-When('Type the name of the city and Click to display', () => {
-    cy.get('.chzn-search > input').type('João Pessoa')
-    cy.get('.active-result').click()
-})
+When("Type the name of the city and Click to display", () => {
+  cy.get(".chzn-search > input").type("João Pessoa");
+  cy.get(".active-result").click();
+});
 
-Then('Shows city data', () => {
-    cy.get('#autoComplete')
-})
-
-
+Then("Shows city data", () => {
+  cy.get("#autoComplete");
+});
